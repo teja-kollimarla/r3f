1ed951b31e3b6eb827bb05610615c10d0a1b9bf2
Kollimarla Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-04-26 22:57:15 +0530

    rotation of a box

---
+++ l2/src/App.jsx
@@ -0,0 +1,38 @@
+import {Canvas,useFrame} from '@react-three/fiber';
+import { useRef } from 'react';
+import { BoxGeometry } from 'three/src/Three.Core.js';
+
+
+function AnimateBox(){
+const boxref=useRef()
+ useFrame(()=>{
+  boxref.current.rotation.x += 0.005;
+  
+  boxref.current.rotation.y += 0.005;
+  
+  boxref.current.rotation.z += 0.005;
+ }) 
+ return(
+<mesh ref={boxref} >
+          <boxGeometry args={[1,1,1]}/>
+          {/* <meshBasicMaterial wireframe color={'red'}/>  don't need light  */}
+          <meshStandardMaterial  color={'red'}/>           {/* need light source */}
+          
+        </mesh>
+ ) 
+
+}
+
+function App() {
+ 
+  return (
+    <div className='canvas-div' style={{}}>
+      <Canvas >        {/* camera={{position:[2,-2,-2]}} */}
+          <AnimateBox/>
+        <directionalLight position={[0,0,0]}/>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App

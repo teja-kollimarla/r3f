9f0c7e505307998379047485eaf3ffaaadd38424
Kollimarla Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-04-26 23:04:14 +0530

    added zoom in and zoom out effect

---
+++ l3/src/App.jsx
@@ -0,0 +1,39 @@
+import {Canvas,useFrame} from '@react-three/fiber';
+import { useRef } from 'react';
+import { BoxGeometry } from 'three/src/Three.Core.js';
+import { FirstPersonControls } from '@react-three/drei';
+
+function AnimateBox(){
+const boxref=useRef()
+ useFrame(()=>{
+  boxref.current.rotation.x += 0.005;
+  
+  boxref.current.rotation.y += 0.005;
+  
+  boxref.current.rotation.z += 0.005;
+ }) 
+ return(
+<mesh ref={boxref} >
+          <FirstPersonControls/>
+          <boxGeometry args={[1,1,1]}/>
+          {/* <meshBasicMaterial wireframe color={'red'}/>  don't need light  */}
+          <meshStandardMaterial  color={'red'}/>           {/* need light source */}
+          
+        </mesh>
+ ) 
+
+}
+
+function App() {
+ 
+  return (
+    <div className='canvas-div' style={{}}>
+      <Canvas >        {/* camera={{position:[2,-2,-2]}} */}
+          <AnimateBox/>
+        <directionalLight position={[0,0,0]}/>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App

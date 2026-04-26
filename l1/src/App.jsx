c4f719629b05c0e961facf80be8e900983de433f
Kollimarla Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-04-26 22:39:29 +0530

    mesh and direction light

---
+++ l1/src/App.jsx
@@ -0,0 +1,22 @@
+import {Canvas} from '@react-three/fiber';
+import { BoxGeometry } from 'three/src/Three.Core.js';
+
+function App() {
+ 
+
+  return (
+    <div className='canvas-div' style={{}}>
+      <Canvas >
+        <mesh>
+          <boxGeometry args={[1,1,3]}/>
+          {/* <meshBasicMaterial wireframe color={'red'}/>  don't need light  */}
+          <meshPhongMaterial wireframe color={'red'}/>           {/* need light source */}
+          
+        </mesh>
+        <directionalLight position={[0,10,110]}/>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
